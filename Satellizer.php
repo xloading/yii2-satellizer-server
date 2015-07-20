5c42d5fdf9de277f93dd847f67c1706a9a7893ce
Welsiton Ferreira <wfcreations@gmail.com>
2015-07-20 12:26:07 -0300

    implement config params

--- Satellizer.php
+++ Satellizer.php
@@ -4,26 +4,97 @@ namespace wfcreations\satellizer;
 
 use Yii;
 use yii\base\Component;
-use wfcreations\satellizer\models\User;
 
 class Satellizer extends Component {
 
     public static $componentName = 'satellizer';
-    public $identityClass;
-
-    public function init() {
-        parent::init();
-
-        if ($this->identityClass === null) {
-            $this->identityClass = User::className();
-        }
-    }
+    public $identityClass = '\wfcreations\satellizer\models\User';
+    public $tokenLifetime = 2 * 7 * 24 * 60 * 60;
+    public $jwtKey;
+    public $facebook = [
+    ];
+    public $google = [
+    ];
+    public $linkedin = [
+    ];
+    public $twitter = [
+    ];
+    public $foursquare = [
+    ];
+    public $github = [
+    ];
 
     /**
      * @return Satellizer get Satelizer component
      */
     public static function getComponent() {
         return Yii::$app->{static::$componentName};
+    }
+
+    public function init() {
+        $this->initFacebookDefaults();
+        $this->initGoogleDefaults();
+        $this->initLinkedinDefaults();
+        $this->initTwitterDefaults();
+        $this->initFoursquareDefaults();
+        $this->initGithubDefaults();
+    }
+
+    public function initFacebookDefaults() {
+        if (!isset($this->facebook['accessTokenUrl'])) {
+            $this->facebook['accessTokenUrl'] = 'https://graph.facebook.com/v2.3/oauth/access_token';
+        }
+        if (!isset($this->facebook['graphApiUrl'])) {
+            $this->facebook['graphApiUrl'] = 'https://graph.facebook.com/v2.3/me';
+        }
+    }
+
+    public function initGoogleDefaults() {
+        if (!isset($this->google['accessTokenUrl'])) {
+            $this->google['accessTokenUrl'] = 'https://accounts.google.com/o/oauth2/token';
+        }
+        if (!isset($this->google['peopleApiUrl'])) {
+            $this->google['peopleApiUrl'] = 'https://www.googleapis.com/plus/v1/people/me/openIdConnect';
+        }
+    }
+
+    public function initLinkedinDefaults() {
+        if (!isset($this->linkedin['accessTokenUrl'])) {
+            $this->linkedin['accessTokenUrl'] = 'https://www.linkedin.com/uas/oauth2/accessToken';
+        }
+        if (!isset($this->linkedin['peopleApiUrl'])) {
+            $this->linkedin['peopleApiUrl'] = 'https://api.linkedin.com/v1/people/~:(id,first-name,last-name,email-address)';
+        }
+    }
+
+    public function initTwitterDefaults() {
+        if (!isset($this->twitter['requestTokenUrl'])) {
+            $this->twitter['requestTokenUrl'] = 'https://api.twitter.com/oauth/request_token';
+        }
+        if (!isset($this->twitter['accessTokenUrl'])) {
+            $this->twitter['accessTokenUrl'] = 'https://api.twitter.com/oauth/access_token';
+        }
+        if (!isset($this->twitter['profileUrl'])) {
+            $this->twitter['profileUrl'] = 'https://api.twitter.com/1.1/users/show.json?screen_name=';
+        }
+    }
+
+    public function initFoursquareDefaults() {
+        if (!isset($this->foursquare['accessTokenUrl'])) {
+            $this->foursquare['accessTokenUrl'] = 'https://foursquare.com/oauth2/access_token';
+        }
+        if (!isset($this->foursquare['userProfileUrl'])) {
+            $this->foursquare['userProfileUrl'] = 'https://api.foursquare.com/v2/users/self';
+        }
+    }
+
+    public function initGithubDefaults() {
+        if (!isset($this->github['accessTokenUrl'])) {
+            $this->github['accessTokenUrl'] = 'https://github.com/login/oauth/access_token';
+        }
+        if (!isset($this->github['userApiUrl'])) {
+            $this->github['userApiUrl'] = 'https://api.github.com/user';
+        }
     }
 
 }
--- actions/FacebookAction.php
+++ actions/FacebookAction.php
@@ -7,29 +7,33 @@ use Firebase\JWT\JWT;
 use GuzzleHttp\Client;
 use yii\base\Action;
 use wfcreations\satellizer\Satellizer;
-use wfcreations\satellizer\models\User;
 
 class FacebookAction extends Action {
 
-    public function run() {
+    protected function createToken($user) {
+        $payload = [
+            'sub' => $user->id,
+            'iat' => time(),
+            'exp' => time() + (2 * 7 * 24 * 60 * 60)
+        ];
+        return JWT::encode($payload, Config::get('app.token_secret'));
+    }
 
+    public function run() {
         if (Yii::$app->getRequest()->isPost) {
             $user_class = Satellizer::getComponent()->identityClass;
-
-            $accessTokenUrl = 'https://graph.facebook.com/v2.3/oauth/access_token';
-            $graphApiUrl = 'https://graph.facebook.com/v2.3/me';
 
             $params = [
                 'code' => Yii::$app->getRequest()->getBodyParam('code'),
                 'client_id' => Yii::$app->getRequest()->getBodyParam('clientId'),
                 'redirect_uri' => Yii::$app->getRequest()->getBodyParam('redirectUri'),
-                'client_secret' => 'ed725d39f9638484672d21b58d786dfa'
+                'client_secret' => Satellizer::getComponent()->facebook['clientSecret'],
             ];
 
             $cliente = new Client();
-            $accessToken = $cliente->get($accessTokenUrl, ['query' => $params])->json();
+            $accessToken = $cliente->get(Satellizer::getComponent()->facebook['accessTokenUrl'], ['query' => $params])->json();
 
-            $profile = $cliente->get($graphApiUrl, ['query' => $accessToken])->json();
+            $profile = $cliente->get(Satellizer::getComponent()->facebook['graphApiUrl'], ['query' => $accessToken])->json();
 
             if (Yii::$app->getRequest()->getHeaders()->get('Authorization')) {
                 $user = $user_class::findOne(['facebook' => $profile['id']]);
@@ -39,7 +43,7 @@ class FacebookAction extends Action {
                 }
 
                 $token = explode(' ', Yii::$app->getRequest()->getHeaders()->getHeaders()->get('Authorization'))[1];
-                $payload = (array) JWT::decode($token, '123456789', ['HS256']);
+                $payload = (array) JWT::decode($token, Satellizer::getComponent()->jwtKey, ['HS256']);
 
                 $user = $user_class::find($payload['sub']);
                 $user->facebook = $profile['id'];
